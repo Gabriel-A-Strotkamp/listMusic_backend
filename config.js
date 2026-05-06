ff47198257903444ea9e2a4147a844705325c825
Gabriel Anael Strotkamp <127111398+Gabriel-A-Strotkamp@users.noreply.github.com>
2026-05-06 11:18:14 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,16 +1,22 @@
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 
-const pool = process.env.DATABASE_URL
-  ? new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
-    })
-  : new Pool({
-      user: 'postgres',
-      host: 'localhost',
-      database: 'BD_listMusic',
-      password: '1808amor',
-      port: 5432
-    });
+const isProduction = process.env.NODE_ENV === 'production'
+
+let pool = null;
+if (isProduction) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL, ssl: {
+      rejectUnauthorized: false,
+    }
+  })
+} else {
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'BD_listMusic',
+    password: 'postgres',
+    port: 5432
+  })
+}
 
 module.exports = pool;
