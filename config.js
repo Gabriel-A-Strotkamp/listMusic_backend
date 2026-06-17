a03a73ca7ec9c813c3b5b288479dfe8f7aa07d3e
Gabriel Anael Strotkamp <127111398+Gabriel-A-Strotkamp@users.noreply.github.com>
2026-06-17 09:58:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
 let pool = null;
+
 if (isProduction) {
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL, ssl: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
       rejectUnauthorized: false,
     }
-  })
+  });
 } else {
   pool = new Pool({
     user: 'postgres',
@@ -16,7 +18,7 @@ if (isProduction) {
     database: 'BD_listMusic',
     password: 'postgres',
     port: 5432
-  })
+  });
 }
 
 module.exports = pool;
