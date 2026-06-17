095f4f120e53be7caf007c9dea8ee88a6c151eb8
Gabriel Anael Strotkamp <127111398+Gabriel-A-Strotkamp@users.noreply.github.com>
2026-06-17 09:27:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 let pool = null;
 if (isProduction) {
-  pool = new Pool({
+  pool = new pool({
     connectionString: process.env.DATABASE_URL, ssl: {
       rejectUnauthorized: false,
     }
   })
 } else {
-  pool = new Pool({
+  pool = new pool({
     user: 'postgres',
     host: 'localhost',
     database: 'BD_listMusic',
