7ecf5d8cdee3bd752deb60aeed8a891d81fb74e5
Gabriel Anael Strotkamp <127111398+Gabriel-A-Strotkamp@users.noreply.github.com>
2026-06-17 09:58:07 -0300

    Update segurancaUseCases.js

--- usecases/segurancaUseCases.js
+++ usecases/segurancaUseCases.js
@@ -1,22 +1,32 @@
-const {pool} = require('../config')
-const Usuario = require('../entities/Usuario')
+const pool = require('../config');
+const Usuario = require('../entities/Usuario');
 
 const autenticaUsuarioDB = async (body) => {
-    try {           
-        const { email, senha } = body
-        const results = await pool.query(`SELECT * FROM usuarios WHERE email = $1 AND senha = $2`,
-        [email, senha]);
-        
-        if (results.rowCount == 0) {
-            throw "Usuário ou tenha inválidos";
+    try {
+        const { email, senha } = body;
+
+        const results = await pool.query(
+            'SELECT * FROM usuarios WHERE email = $1 AND senha = $2',
+            [email, senha]
+        );
+
+        if (results.rowCount === 0) {
+            throw "Usuário ou senha inválidos";
         }
+
         const usuario = results.rows[0];
-        return new Usuario(usuario.email, usuario.tipo, usuario.telefone, usuario.nome);
+
+        return new Usuario(
+            usuario.email,
+            usuario.tipo,
+            usuario.telefone,
+            usuario.nome
+        );
     } catch (err) {
         throw "Erro ao autenticar o usuário: " + err;
-    }    
-}
+    }
+};
 
 module.exports = {
     autenticaUsuarioDB
-}
+};
